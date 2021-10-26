12c4e2d9f07154ab34ace4b0a27baa5d6de605a9
Jhon Moreno <jhonjairomoreno90@gmail.com>
2021-10-26 09:36:42 -0500

    update method update class client

--- src/main/java/Retos/Servicio/ServicioCliente.java
+++ src/main/java/Retos/Servicio/ServicioCliente.java
@@ -54,6 +54,9 @@ public class ServicioCliente {
                 if(client.getPassword()!=null){
                     e.get().setPassword(client.getPassword());
                 }
+                if(client.getEmail()!=null){
+                    e.get().setEmail(client.getEmail());
+                }
                 metodosCrud.save(e.get());
                 return e.get();
             }else{
