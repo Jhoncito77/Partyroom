38b1aebc1b1a4450717c5f854506d324b25c677d
Jhon Moreno <jhonjairomoreno90@gmail.com>
2021-11-23 09:57:56 -0500

    actualizacion de java.doc para serviceReservation.java

--- src/main/java/Retos/Servicio/ServicioReservacion.java
+++ src/main/java/Retos/Servicio/ServicioReservacion.java
@@ -13,22 +13,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- *
+ * Clase empleada para usar los servicios Crud del repository
  * @author 57300
  */
 @Service
 public class ServicioReservacion {
     @Autowired
+    /**
+     * parametro para implementacion de metodos crud
+     * @param parametro empleado para usar los metodos crud del repositorio
+     */
     private RepositorioReservacion metodosCrud;
-    
+    /**
+     * Fundion empleada para traer todas las reservas
+     * @return Todas las reservas registradas
+     */
     public List<Reservacion> getAll(){
         return metodosCrud.getAll();
     }
-
+/**
+ * Funcion empleada para llamar a una reservacion por su id
+ * @param reservationId Id de la reservacion a llamar
+ * @return Reservacion obtenida por id
+ */
     public Optional<Reservacion> getReservation(int reservationId) {
         return metodosCrud.getReservation(reservationId);
     }
-
+/**
+ * Funcion empleada para guardar reservaciones, para ello usamos un objeto de la clase reservation
+ * @param reservation objeto de la clase reservation a guardar como reservacion
+ * @return objeto reservacion guardada
+ */
     public Reservacion save(Reservacion reservation){
         if(reservation.getIdReservation()==null){
             return metodosCrud.save(reservation);
@@ -41,6 +56,11 @@ public class ServicioReservacion {
             }
         }
     }
+    /**
+     * funcion empleada para editar reservaciones por id
+     * @param reservacion contiene los parametros del objeto reservation a modificar, incluyendo el id
+     * @return el objeto reservation editado
+     */
 public Reservacion update(Reservacion reservacion){
         if(reservacion.getIdReservation()!=null){
             Optional<Reservacion> e= metodosCrud.getReservation(reservacion.getIdReservation());
@@ -65,7 +85,11 @@ public Reservacion update(Reservacion reservacion){
         }
     }
 
-    
+    /**
+     * Fundion empleada para eliminar registros por id
+     * @param reservationId Id del registro a eliminar
+     * @return true si se elimino el registro con exito, false si no se pudo realizar la accion de eliminar el registro
+     */
     public boolean deleteReservation(int reservationId) {
         Boolean aBoolean = getReservation(reservationId).map(reservation -> {
             metodosCrud.delete(reservation);
